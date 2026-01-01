cef7a2ba0371185278c9523def141ca385f7a209
Shani Kotadiya <shanikotadiya@gmail.com>
2026-01-01 23:03:06 +0530

    udpate

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print("hello world")
+# first.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Add 2+3 =", add(2, 3))
+    print("Subtract 5-2 =", subtract(5, 2))
